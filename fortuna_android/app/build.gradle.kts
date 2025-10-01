799d400fa33cee95f8b8dea5848e6e354d8b1000
LHJ1q <seers.louis@gmail.com>
2025-10-01 21:44:33 +0900

    fix: library version catalog arrange

--- fortuna_android/app/build.gradle.kts
+++ fortuna_android/app/build.gradle.kts
@@ -79,16 +79,22 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.google.ar:core:1.50.0")
-    implementation("com.google.android.gms:play-services-auth:21.3.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.activity:activity-ktx:1.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(libs.google.ar.core)
+    implementation(libs.play.services.auth)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    // Dependency for CameraX API
+    implementation(libs.bundles.camerax)
+    // Dependency for fragment navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    // Dependency for ExifInterface
+    implementation(libs.androidx.exifinterface)
+    // Dependency for Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.okhttp.logging.interceptor)
+    // Dependency for image processing with Glide
+    implementation(libs.glide)
 
 }
